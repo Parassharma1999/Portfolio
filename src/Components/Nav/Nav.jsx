2fddf47e65ca2ab4868f8d71f2932827b3eca743
Paras <sparas1999@gmail.com>
2022-06-22 23:21:30 +0530

    Navbar developed

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {AiOutlineHome,AiOutlineUser,AiOutlineMessage} from "react-icons/ai"
+import {BiBook} from "react-icons/bi"
+import {RiServiceLine} from "react-icons/ri"
+
+
 import "./Nav.css"
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><AiOutlineMessage/></a>
+    </nav>
   )
 }
 
