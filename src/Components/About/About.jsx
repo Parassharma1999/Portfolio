1648c2a77c2890cbe7bdb6f3b46d44101750839a
Paras <sparas1999@gmail.com>
2022-06-23 23:54:41 +0530

    Navbar Completed

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
-
+import ME from "../../Assets/me-about.jpg"
+import {FaAward} from "react-icons/fa"
+import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+      <div className="conatiner about__conatainer">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="AboutImage" />
+          </div>
+        </div>
+        
+          <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+               <small>1 internship</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+               <small>10+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+               <small>3+ Completed</small>
+            </article>
+          </div>
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus perspiciatis doloremque facilis similique amet incidunt veritatis, quam aspernatur assumenda quaerat ullam rem delectus quod est obcaecati ex aperiam. Ex, similique.</p>
+
+      <a href="#contact" className='btn btn-primary' >Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default About
+export default About 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import {AiOutlineHome,AiOutlineUser,AiOutlineMessage} from "react-icons/ai"
-import {BiBook} from "react-icons/bi"
-import {RiServiceLine} from "react-icons/ri"
+import React from "react";
+import { AiOutlineHome, AiOutlineUser, AiOutlineMessage } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { useState } from "react";
 
-
-import "./Nav.css"
+import "./Nav.css";
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><AiOutlineMessage/></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
